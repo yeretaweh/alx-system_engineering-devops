46ca56ce9f59d762c1aaa2f2982c4c47b6b9d7a0
warLite <yeretaweh@gmail.com>
2023-05-04 19:06:40 -0700

    update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,17 @@
 #!/usr/bin/env ruby
 # A regex expression to that accepts one argument and pass it to a regular matching method
 
+regex = /hbt*n/
+input_string = "hbn hbtn hbttn hbtttn hbttttn hbtttttn hbttttttn"
 
-puts ARGV[0].scan(/School/).join
+matches = input_string.scan(regex)
+
+if matches.empty?
+  puts "No matches found."
+else
+  puts "Matches found:"
+  matches.each do |match|
+    puts match
+  end
+end
+i
